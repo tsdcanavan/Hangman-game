178ac9e4d4907f00fc3b3681aa557fa5fe8cdc12
unknown <tsdcanavan@yahoo.com>
2017-08-12 09:32:00 -0400

    update some files

--- assets/javascript/inputtest.js
+++
@@ -1,9 +0,0 @@
-
-
-console.log('Starting ...');
-$("#keyInput").keypress(function(e) {
-    if (e.which !== 0) {
-        alert("Charcter was typed. It was: " + String.fromCharCode(e.which));
-    }
-});
-console.log('Ending ...');
